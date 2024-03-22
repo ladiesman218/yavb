615f2582e067542a9c2642cbd7300f0bfa9dc9b4
Gao Lei <7885834+ladiesman218@users.noreply.github.com>
2024-03-22 10:22:01 +0800

    Update AuthControllerTests.swift

--- Tests/AppTests/AuthControllerTests.swift
+++ Tests/AppTests/AuthControllerTests.swift
@@ -1,57 +1,80 @@
-// //
-// //  ControllerTests.swift
-// //
-// //
-// //  Created by Lei Gao on 2024/3/19.
-// //
+//
+//  ControllerTests.swift
+//
+//
+//  Created by Lei Gao on 2024/3/19.
+//
 
-// import Vapor
-// import XCTVapor
-// import HTTPTypes
+import Vapor
+import XCTVapor
+import HTTPTypes
 
-// @testable import App
-// final class AuthControllerTests: XCTestCase {
-//     override class func setUp() {
-//         super.setUp()
-//         print("setting up")
-//     }
-//     override class func tearDown() {
-//         super.tearDown()
-//         print("tearing down")
-//     }
+@testable import App
+final class AuthControllerTests: XCTestCase {    
+    var app: Application!
+    override func setUp() async throws {
+        try await super.setUp()
+        app = try Application.testable()
+    }
     
-//     let pathString = String(try! Servers.server1().relativePath.dropFirst() + "/auth")
+    override func tearDown() {
+        super.tearDown()
+        app.shutdown()
+        app = nil
+    }
     
-//     func testRegister() async throws {
-//         let app = Application(.testing)
-//         try await configure(app)
-//         defer { app.shutdown() }
-        
-//         let endpoint = pathString + "/register"
-//         try app.testable().test(.GET, "openapi") { response in
-//             XCTAssertEqual(response.status.code, 301)
-//         }
-//         try app.testable(method: .running(port: 8082)).test(.POST, endpoint, beforeRequest: { req in
-//             try req.content.encode([
-//                 "email": "asldkfj@asdf.csssssom",
-//                 "username": "vapor",
-//                 "password1": "asdfasdf",
-//                 "password2": "asdfasdf"
-//             ], as: .json)
-//         }) { res in
-//             print("😁")
-//             print(res.body.string)
-//             XCTAssertEqual(res.status, .created)
-// //            XCTAssertEqual(res.body.string, Components.Schemas.ServerConflictError.Email_space_has_space_been_space_taken.rawValue)
-//         }
-// //        .test(.POST, "/register") { res in
-// //            XCTAssertEqual(res.status, .unprocessableEntity)
-// //            XCTAssertContains(res.body.string.replacingOccurrences(of: "\\", with: ""), "Missing \"Content-Type\" header")
-// //        }.test(.POST, "/register", headers: ["Content-Type":"application/json"]) { res in
-// //            XCTAssertEqual(res.status, .unprocessableEntity)
-// //            XCTAssertContains(res.body.string, "Empty Body")
-// //        }
-//     }
+    let pathString = String(try! Servers.server1().relativePath.dropFirst() + "/auth")
+    
+    func testRegister() async throws {
+        let endpoint = pathString + "/register"
+        try app.testable().test(.GET, "openapi") { response in
+            XCTAssertEqual(response.status.code, 301)
+        }
+        try app.testable().test(.POST, endpoint, beforeRequest: { req in
+            try req.content.encode([
+                "email": "asdf@1234.com",
+                "username": "asdf",
+                "password1": "asdfasdf",
+                "password2": "asdfasdfs"
+            ], as: .json)
+        }) { res in
+            XCTAssertEqual(res.status, .badRequest)
+            XCTAssertContains(res.body.string, "Passwords")
+        }
+        .test(.POST, endpoint, beforeRequest: { req in
+            try req.content.encode([
+                "email": "asdf@1234.com",
+                "username": "asdf",
+                "password1": "asdfasdf",
+                "password2": "asdfasdf"
+            ], as: .json)
+        }) { res in
+            XCTAssertEqual(res.status, .created)
+            XCTAssertTrue(res.body.readableBytesView.isEmpty)
+        }
+        .test(.POST, endpoint, beforeRequest: { req in
+            try req.content.encode([
+                "email": "asdf@1234.com",
+                "username": "vapor",
+                "password1": "asdfasdf",
+                "password2": "asdfasdf"
+            ], as: .json)
+        }) { res in
+            XCTAssertEqual(res.status, .conflict)
+            XCTAssertEqual(res.body.string, Components.Schemas.ServerConflictError.Email_space_has_space_been_space_taken.rawValue)
+        }
+        .test(.POST, endpoint, beforeRequest: { req in
+            try req.content.encode([
+                "email": "vapor@gmail.com",
+                "username": "asdf",
+                "password1": "asdfasdf",
+                "password2": "asdfasdf"
+            ], as: .json)
+        }) { res in
+            XCTAssertEqual(res.status, .conflict)
+            XCTAssertEqual(res.body.string, Components.Schemas.ServerConflictError.Username_space_has_space_been_space_taken.rawValue)
+        }
+    }
     
     
-// }
+}
