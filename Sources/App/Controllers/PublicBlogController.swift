8efb6c93f846243a6b2e0e26c3f36cc64bccc35a
Gao Lei <duncej@gmail.com>
2024-09-14 17:12:52 +0800

    Changed post's isPublished boolean value to enum Status

--- Sources/App/Controllers/PublicBlogController.swift
+++ Sources/App/Controllers/PublicBlogController.swift
@@ -7,23 +7,23 @@ struct PublicBlogController: RouteCollection {
         blogRoute.get(use: getRecent)
     }
     
-    func getRecent(_ req: Request) async throws -> [BlogPost.DTO] {
+    func getRecent(_ req: Request) async throws -> [BlogPost.DetailDTO] {
         let posts = try await BlogPost.query(on: req.db).group(.and, { group in
-            group.filter(\.$isPublished == true)
+            group.filter(\.$status == .published)
             group.filter(\.$type == .post)
-        }).sort(\.$updatedAt, .descending).with(\.$tags).with(\.$author).paginate(for: req)
-        let dtos = try posts.items.map { try $0.dto }
+        }).sort(\.$updatedAt, .descending).with(\.$tags).with(\.$author).with(\.$comments).paginate(for: req)
+        let dtos = try posts.items.map { try $0.detailDTO }
         
         return dtos
     }
     
-    func getPost(_ req: Request) async throws -> BlogPost.DTO {
+    func getPost(_ req: Request) async throws -> BlogPost.DetailDTO {
         guard let id = req.parameters.get("id", as: BlogPost.IDValue.self) else {
             throw Abort(.badRequest)
         }
-        guard let post = try await BlogPost.query(on: req.db).filter(\.$id == id).with(\.$author).with(\.$tags).first() else {
+        guard let post = try await BlogPost.query(on: req.db).filter(\.$id == id).filter(\.$status == .published).with(\.$author).with(\.$tags).with(\.$comments).first() else {
             throw Abort(.notFound)
         }
-        return try post.dto
+        return try post.detailDTO
     }
 }
--- Sources/App/Models/BlogPost.swift
+++ Sources/App/Models/BlogPost.swift
@@ -11,81 +11,131 @@ final class BlogPost: Model, Content, @unchecked Sendable {
     @Field(key: FieldKeys.content) var content: String
     @Parent(key: FieldKeys.authorID) var author: User
     @Enum(key: FieldKeys.type) var type: PostType
-    @Field(key: FieldKeys.isPublished) var isPublished: Bool
+    @Enum(key: FieldKeys.status) var status: Status
     @Timestamp(key: FieldKeys.createdAt, on: .create) var createdAt: Date?
     @Timestamp(key: FieldKeys.updatedAt, on: .update) var updatedAt: Date? 
     @Siblings(through: BlogTagPivot.self, from: \.$blog, to: \.$tag) var tags: [Tag]
+    @Children(for: \Comment.$post) var comments: [Comment]
     
     init() {}
     
-    init(id: BlogPost.IDValue? = nil, title: String, excerpt: String, content: String, authorID: User.IDValue, type: PostType = .post, isPublished: Bool = true) {
+    init(id: BlogPost.IDValue? = nil, title: String, excerpt: String, content: String, authorID: User.IDValue, type: PostType = .post, status: Status) {
         self.id = id
         self.title = title
         self.excerpt = excerpt
         self.content = content
         self.$author.id = authorID
         self.type = type
-        self.isPublished = isPublished
+        self.status = status
+    }
+    
+    convenience init(from req: Request) throws {
+        let user = try req.auth.require(User.self)
+        guard user.activated else { throw Abort(.unauthorized, reason: "Un-verified email address") }
+        guard user.role.authorizations.contains(.addPost) else { throw Abort(.unauthorized, reason: "Do not have authorization to add post") }
+        let input = try req.content.decode(BlogPost.CreateInput.self)
+        guard input.type != .page || user.role == .webmaster else { throw Abort(.unauthorized) }
+        
+        let status: Status
+        switch input.status {
+            case .rejected, .pendingReview:
+                throw Abort(.badRequest, reason: "Available status for new post are: \(BlogPost.Status.draft) and \(BlogPost.Status.published)")
+            case .published:
+                status = (postsNeedReview && !user.role.authorizations.contains(.reviewPost)) ? .pendingReview : .published
+            default:
+                status = .draft
+        }
+        
+        self.init(title: input.title, excerpt: input.excerpt ?? "", content: input.content, authorID: try user.requireID(), type: input.type ?? .post, status: status)
     }
 }
 
 extension BlogPost {
+    enum Status: String, Codable, CaseIterable {
+        // Indicates a post is in editing process. Both owner of the post, and any other users with higher level of authorizations than the owner can change a post to draft from any previous status.
+        case draft
+        // Indicates a post is waiting for higher level users' review to become published. When postsNeedReview is on, every time an author(those doesn't have .reviewPosts authorization) creates/updates a post of which status is not draft, should become pendingReview automatically. Any other user with higher level of management auth than the owner can change posts of which status is not draft, to pendingReview despite whether postsNeedReview is on/off.
+        case pendingReview
+        // Indicates a post can be viewed publicly. Normal author can change a post's status to published from any status other than rejected and pendingReview when postsNeedReview is off, if it's on, manually set status to pendingReview dispite previous status. Higher level of users can change a post to published from any status other than draft.
+        case published
+        // Indicates a post has gone through review process, but failed to meet the criteria to be published. Only users with higher level than the author of the post can change a post to rejected from any status other than draft.
+        case rejected
+    }
+    
     enum FieldKeys {
         static let title: FieldKey = .init(stringLiteral: "title")
         static let excerpt: FieldKey = .init(stringLiteral: "excerpt")
         static let content: FieldKey = .init(stringLiteral: "content")
         static let authorID: FieldKey = .init(stringLiteral: "author_id")
         static let type: FieldKey = .init(stringLiteral: "type") // Post or page
-        static let isPublished: FieldKey = .init(stringLiteral: "published")
-        static let createdAt: FieldKey = .init(stringLiteral: "create_at")
-        static let updatedAt: FieldKey = .init(stringLiteral: "update_at")
+        static let status: FieldKey = .init(stringLiteral: "status")
+        static let createdAt: FieldKey = .init(stringLiteral: "created_at")
+        static let updatedAt: FieldKey = .init(stringLiteral: "updated_at")
     }
     
     enum PostType: String, Codable {
         case post
         case page
     }
-    struct DTO: Content {
-        let id: UUID
-        let title: String
-        let excerpt: String
-        let content: String
-        let auther: AuthorDTO
-        let type: PostType
-        let isPublished: Bool
-        let createdAt: Date?
-        let updatedAt: Date?
-        let tags: [Tag]
-    }
-    
-    // Less info than User.DTO
-    struct AuthorDTO: Codable {
-        let id: UUID
-        let username: String
-    }
-    var dto: DTO {
-        get throws {
-            let id = try requireID()
-            let authorDTO = AuthorDTO(id: try author.requireID(), username: author.username)
-            return .init(id: id, title: title, excerpt: excerpt, content: content, auther: authorDTO, type: type, isPublished: isPublished, createdAt: createdAt, updatedAt: updatedAt, tags: tags)
-        }
-    }
-    
+}
+
+extension BlogPost {
     struct CreateInput: Content {
         let title: String
-        let content: String
         let excerpt: String?
-        let tags: [String]?
-        let type: PostType.RawValue?
-        let isPublished: Bool?
+        let content: String
+        let type: PostType?
+        var status: Status
     }
     
     struct UpdateInput: Content {
         let title: String?
         let excerpt: String?
         let content: String?
-        let tags: [String]?
-        let type: PostType.RawValue?
-        let isPublished: Bool?
+        let type: PostType?
+        let status: Status?
+    }
+}
+
+extension BlogPost {
+    struct DetailDTO: Content {
+        let id: BlogPost.IDValue
+        let title: String
+        let excerpt: String
+        let content: String
+        let auther: User.AuthorDTO
+        let status: Status
+        let createdAt: Date?
+        let updatedAt: Date?
+        let tags: [Tag]
+    }
+    
+    var detailDTO: DetailDTO {
+        get throws {
+            let id = try requireID()
+            let authorDTO = try author.authorDTO
+            return .init(id: id, title: title, excerpt: excerpt, content: content, auther: authorDTO, status: status, createdAt: createdAt, updatedAt: updatedAt, tags: tags)
+        }
+    }
+    
+    struct ListDTO: Content {
+        let id: BlogPost.IDValue
+        let title: String
+        let updatedAt: Date
+        let commentCount: Int
+        let authorName: String
+        let status: Status
+        // Multiple tags is concatenated into one string, separated by commas
+        let tags: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case title
+            case updatedAt = "updated_at"
+            case commentCount = "comment_count"
+            case authorName = "author_name"
+            case status = "status"
+            case tags = "tags"
+        }
     }
 }
